f95193f99d5e97b45473dbde5e178d3f3f025b63
marlon2219 <marloncajamarca@gmail.com>
2021-09-27 18:39:34 +0200

    Create Prueba_github.java
    iniciando proyecto java

---
+++ prueba_github/src/prueba_github/Prueba_github.java
@@ -0,0 +1,13 @@
+
+package prueba_github;
+
+public class Prueba_github {
+
+    public static void main(String[] args) {
+        int num = 10;
+        int num2 = 22;
+        System.out.println("la suma es" +(num+num2) );
+        
+    }
+    
+}
